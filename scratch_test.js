a33357a828d27f7b4daa29dcabf832ee8f5042eb
“FirmanGanteng08” <firmandn8@gmail.com>
2026-05-03 16:30:57 +0700

    first commi

--- scratch_test.js
+++
@@ -1,9 +0,0 @@
-fetch('http://localhost:3000/api/music/stream?id=JGwWNGJdvx8')
-  .then(res => {
-    console.log('Status:', res.status);
-    console.log('Content-Type:', res.headers.get('content-type'));
-    console.log('Content-Length:', res.headers.get('content-length'));
-    if (!res.ok) return res.json().then(d => console.log('Error body:', d));
-    console.log('SUCCESS - Audio stream is working!');
-  })
-  .catch(console.error);
